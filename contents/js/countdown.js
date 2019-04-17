1ea552446bd8d2b535feec07a1040ae62139f53c
Robert Ferris <robert.ferris@codedx.com>
2019-04-16 22:25:03 -0400

    Fix countdown

--- contents/js/countdown.js
+++ contents/js/countdown.js
@@ -3,7 +3,7 @@ const countdown = require('countdown')
 countdown.setLabels(null, null, ', and ', ', ', '', null)
 
 var timerId = countdown(
-	new Date(2019, 9, 13, 16, 30),
+	1568406600000, //new Date(2019, 9, 13, 16, 30),
 	function(ts) { document.getElementById('countdown').innerHTML = 'In ' + ts.toHTML("strong") + '!'; },
 	countdown.MONTHS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS,
 	3
